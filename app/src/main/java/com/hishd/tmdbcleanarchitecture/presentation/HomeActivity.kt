e1c22da00eaf003d2c424879129b87f7e18d2426
Hishara Dilshan <hish2k15@gmail.com>
2022-07-13 23:41:21 +0530

    Added Home Activity Layout

--- app/src/main/java/com/hishd/tmdbcleanarchitecture/presentation/HomeActivity.kt
+++ app/src/main/java/com/hishd/tmdbcleanarchitecture/presentation/HomeActivity.kt
@@ -3,10 +3,15 @@ package com.hishd.tmdbcleanarchitecture.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hishd.tmdbcleanarchitecture.R
+import com.hishd.tmdbcleanarchitecture.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
